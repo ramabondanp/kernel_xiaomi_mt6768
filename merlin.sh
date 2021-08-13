94859ea3a25082d097458d11aca7e468722d033b
Arafattex <arafattex45558@gmail.com>
2021-08-14 02:51:11 +0600

    script file

---
+++ merlin.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Hi Merlin user just wait and watch "
+
+mkdir outM
+export ARCH=arm64
+export SUBARCH=arm64
+export DTC_EXT=dtc
+make O=outM ARCH=arm64 merlin_defconfig
+export PATH="${PWD}/clang-13/bin:${PWD}/clang-13/toolchain/bin:${PWD}/clang-13/toolchain32/bin:${PATH}"
+make -j$(nproc --all) O=outM \
+                       ARCH=arm64 \
+                  CC=clang \
+                  CLANG_TRIPLE=aarch64-linux-gnu- \
+                  CROSS_COMPILE=aarch64-linux-android- \
+                  CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                  LD=ld.lld \
+                  OBJDUMP=llvm-objdump \
+                  STRIP=llvm-strip \
+                  NM=llvm-nm \
+                  OBJCOPY=llvm-objcopy 2> FIXME.txt
+bp=${PWD}/outL
+DATE=$(date "+%Y%m%d-%H%M")
+ZIPNAME="Shas-Dream-Merlin-R-vendor"
+cd ${PWD}/AnyKernel3-master
+rm *.zip *-dtb *dtbo.img
+cp $bp/arch/arm64/boot/Image.gz-dtb .
+cp $bp/arch/arm64/boot/dtbo.img .
+zip -r9 "$ZIPNAME"-"${DATE}".zip *
+cd - || exit
