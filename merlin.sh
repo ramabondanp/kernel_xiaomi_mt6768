c61f122f7f0d7468ecedce3891bcfcdf72b8f2e9
nil <shas45558@gmail.com>
2021-08-15 13:05:10 -0400

    support for merlin

--- merlin.sh
+++ merlin.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+!/bin/bash
 
-echo "Hi Merlin user just wait and watch "
+echo "Hi merlin user just wait and watch "
 
 mkdir outM
 export ARCH=arm64
@@ -9,17 +9,19 @@ export DTC_EXT=dtc
 make O=outM ARCH=arm64 merlin_defconfig
 export PATH="${PWD}/clang-13/bin:${PWD}/toolchain64/bin:${PWD}/toolchain32/bin:${PATH}"
 make -j$(nproc --all) O=outM \
-                       ARCH=arm64 \
+                  ARCH=arm64 \
                   CC=clang \
                   CLANG_TRIPLE=aarch64-linux-gnu- \
                   CROSS_COMPILE=aarch64-linux-android- \
                   CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-bp=${PWD}/outL
+                  LD=ld.lld \
+                  NM=llvm-nm \
+                  OBJCOPY=llvm-objcopy 
+bp=${PWD}/outM
 DATE=$(date "+%Y%m%d-%H%M")
 ZIPNAME="Shas-Dream-Merlin-R-vendor"
 cd ${PWD}/AnyKernel3-master
-rm *.zip *-dtb *dtbo.img
+rm *.zip *-dtb 
 cp $bp/arch/arm64/boot/Image.gz-dtb .
-cp $bp/arch/arm64/boot/dtbo.img .
 zip -r9 "$ZIPNAME"-"${DATE}".zip *
 cd - || exit
